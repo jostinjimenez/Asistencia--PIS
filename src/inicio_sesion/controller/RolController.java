5e0c5b99f44328229f0ac9ca2d6c97e0ec5cdeaa
Jostin Jimenez <jostin.jimenez@unl.edu.ec>
2023-12-17 00:13:57 -0500

    rol y cuenta

--- src/inicio_sesion/controller/RolController.java
+++ src/inicio_sesion/controller/RolController.java
@@ -1,25 +1,25 @@
-package inicio_sesion.controller;
-
-import model.Rol;
-
-public class RolController {
-    private ListaEnlazada<Rol> roles;
-    private Rol rol;
-
-    public ListaEnlazada<Rol> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(ListaEnlazada<Rol> roles) {
-        this.roles = roles;
-    }
-
-    public Rol getRol() {
-        return rol;
-    }
-
-    public void setRol(Rol rol) {
-        this.rol = rol;
-    }
-}
-
+//package inicio_sesion.controller;
+//
+//import model.Rol;
+//
+//public class RolController {
+//    private ListaEnlazada<Rol> roles;
+//    private Rol rol;
+//
+//    public ListaEnlazada<Rol> getRoles() {
+//        return roles;
+//    }
+//
+//    public void setRoles(ListaEnlazada<Rol> roles) {
+//        this.roles = roles;
+//    }
+//
+//    public Rol getRol() {
+//        return rol;
+//    }
+//
+//    public void setRol(Rol rol) {
+//        this.rol = rol;
+//    }
+//}
+//
