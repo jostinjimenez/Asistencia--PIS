d307f95a417a2516d3f22c62903d2885d79cea54
Jostin Jimenez <jostin.jimenez@unl.edu.ec>
2023-12-17 17:21:43 -0500

    relaciones y toString

--- src/model/Estudiante.java
+++ src/model/Estudiante.java
@@ -1,15 +1,12 @@
 package model;
 
-public class Estudiante extends Persona{
+public class Estudiante extends Persona {
     private String direccion;
     private String etnia;
     private Double titulo_bachiller;
 
-    public Estudiante() {
-    }
-
-    public Estudiante(String direccion, String etnia, Double titulo_bachiller, Integer id, String nombre, String correo_personal, String fecha_nacimiento, String telefono, String dni) {
-        super(id, nombre, correo_personal, fecha_nacimiento, telefono, dni);
+    public Estudiante(Integer id, String nombre, String apellido, String correoPersonal, String fechaNacimiento, String telefono, String dni, String direccion, String etnia, Double titulo_bachiller) {
+        super(id, nombre, apellido, correoPersonal, fechaNacimiento, telefono, dni);
         this.direccion = direccion;
         this.etnia = etnia;
         this.titulo_bachiller = titulo_bachiller;
@@ -37,5 +34,10 @@ public class Estudiante extends Persona{
 
     public void setTitulo_bachiller(Double titulo_bachiller) {
         this.titulo_bachiller = titulo_bachiller;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
