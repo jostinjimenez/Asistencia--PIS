6afcad4518f40140cbc521b8d567aa1c834931cb
Jostin Jimenez <jostin.jimenez@unl.edu.ec>
2023-12-16 23:40:34 -0500

    Dao y tdas

---
+++ src/DAO/TransferObject.java
@@ -0,0 +1,14 @@
+package DAO;
+
+
+import tda_listas.ListaEnlazada;
+
+public interface TransferObject<T> {
+    public Boolean save(T data);
+
+    public Boolean update(T data, Integer index);
+
+    public ListaEnlazada<T> list_All();
+
+    public T find(Integer id);
+}
---
+++ src/tda_listas/exceptions/LlenoException.java
@@ -0,0 +1,11 @@
+package tda_listas.exceptions;
+
+public class LlenoException extends Exception{
+
+    public LlenoException() {
+    }
+
+    public LlenoException(String msg) {
+        super(msg);
+    }
+}
