a286055281c4e95cd047a6b8f123b04c55b826cc
Jostin Santiago Jimenez Ulloa <jostin.jimenez@unl.edu.ec>
2023-12-17 21:38:01 -0500

    plantilla

---
+++ src/plantilla/forms/Frm_Principal.java
@@ -0,0 +1,64 @@
+package plantilla.forms;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import javax.swing.UIManager;
+
+public class Frm_Principal extends javax.swing.JFrame {
+
+    public Frm_Principal() {
+        initComponents();
+
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        bg_panel = new javax.swing.JPanel();
+        menu1 = new plantilla.components.Menu();
+        roundPanel1 = new plantilla.swing.RoundPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        bg_panel.setBackground(new java.awt.Color(21, 21, 21));
+        bg_panel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        bg_panel.add(menu1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 210, 620));
+
+        roundPanel1.setBackground(new java.awt.Color(51, 51, 51));
+        bg_panel.add(roundPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 10, 1040, 620));
+
+        getContentPane().add(bg_panel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1280, 640));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        }
+        catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new Frm_Principal().setVisible(true);
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel bg_panel;
+    private plantilla.components.Menu menu1;
+    private plantilla.swing.RoundPanel roundPanel1;
+    // End of variables declaration//GEN-END:variables
+
+}
