bcf71bfa2fcaccd78c695b1cdaa65ac950340d2c
Jostin Jimenez <jostin.jimenez@unl.edu.ec>
2023-12-19 00:25:34 -0500

    interfaz - usuario

--- src/modulo_1/usuario_rol/view/Frm_Usuarios.java
+++ src/modulo_1/usuario_rol/view/Frm_Usuarios.java
@@ -1,11 +1,12 @@
-package plantilla.forms;
+package modulo_1.usuario_rol.view;
 
+import plantilla.forms.*;
 import com.formdev.flatlaf.FlatDarkLaf;
 import javax.swing.UIManager;
 
-public class Frm_Principal extends javax.swing.JFrame {
+public class Frm_Usuarios extends javax.swing.JFrame {
 
-    public Frm_Principal() {
+    public Frm_Usuarios() {
         initComponents();
 
         this.setLocationRelativeTo(null);
@@ -19,6 +20,12 @@ public class Frm_Principal extends javax.swing.JFrame {
         bg_panel = new javax.swing.JPanel();
         menu1 = new plantilla.components.Menu();
         roundPanel1 = new plantilla.swing.RoundPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -28,6 +35,42 @@ public class Frm_Principal extends javax.swing.JFrame {
         bg_panel.add(menu1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 210, 620));
 
         roundPanel1.setBackground(new java.awt.Color(51, 51, 51));
+        roundPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Roboto", 1, 30)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 102, 102));
+        jLabel1.setText("Usuarios");
+        roundPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 30, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Buscar");
+        roundPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(930, 100, 60, 30));
+
+        jTextField1.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
+        jTextField1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        roundPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 140, 240, 30));
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        roundPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 210, 1000, 390));
+
+        jButton1.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
+        jButton1.setText("Nuevo");
+        roundPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 150, 110, 30));
+
         bg_panel.add(roundPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 10, 1040, 620));
 
         getContentPane().add(bg_panel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1280, 640));
@@ -51,12 +94,18 @@ public class Frm_Principal extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
-            new Frm_Principal().setVisible(true);
+            new Frm_Usuarios().setVisible(true);
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel bg_panel;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField jTextField1;
     private plantilla.components.Menu menu1;
     private plantilla.swing.RoundPanel roundPanel1;
     // End of variables declaration//GEN-END:variables
