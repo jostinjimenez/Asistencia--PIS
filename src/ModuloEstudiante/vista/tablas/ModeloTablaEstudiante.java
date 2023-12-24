49434a8f422a4be3ba4a7736a852335cef650f8e
BryanTroya <bryan.troya@unl.edu.ec>
2023-12-23 20:07:15 -0500

    ModeloTablaEstudiante

---
+++ src/ModuloEstudiante/vista/tablas/ModeloTablaEstudiante.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModuloEstudiante.vista.tablas;
+
+import tda_listas.ListaEnlazada;
+import javax.swing.event.EventListenerList;
+import javax.swing.table.AbstractTableModel;
+import model.Estudiante;
+
+/**
+ *
+ * @author santiago
+ */
+public class ModeloTablaEstudiante extends AbstractTableModel {
+
+    private ListaEnlazada<Estudiante> estudiante = new ListaEnlazada<>();
+    Estudiante instancia = new Estudiante();
+    
+    
+
+    public ListaEnlazada<Estudiante> getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(ListaEnlazada<Estudiante> estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public EventListenerList getListenerList() {
+        return listenerList;
+    }
+
+    public void setListenerList(EventListenerList listenerList) {
+        this.listenerList = listenerList;
+    }
+    
+    
+
+    @Override
+    public int getRowCount() {
+        if (estudiante != null) {
+            return estudiante.getSize();
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 9;
+    }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        Estudiante est = null;
+        
+        try {
+            est = estudiante.get(row);
+        } catch (Exception e) {
+        }
+        
+        switch (col) {
+            case 0:
+                return (est != null) ? est.getId(): " ";
+            case 1:
+                return (est != null) ? est.getNombre() : " ";
+            case 2:
+                return (est != null) ? est.getFechaNacimiento() : " ";
+            case 3:
+                return (est != null) ? est.getCorreo() : " ";
+            case 4:
+                return (est != null) ? est.getCedula() : " ";
+            case 5:
+                return (est != null) ? est.getTelefono() : " ";
+            case 6:
+                return (est != null) ? est.getEtnia() : " ";
+            case 7:
+                return (est != null) ? est.isTituloBachiller() : " ";
+            case 8:
+                return (est != null) ? est.getProvincia() : " ";
+            
+
+            default:
+                return null;
+        }
+    }
+    
+    @Override
+    public String getColumnName(int col) {
+        switch (col) {
+            case 0:
+                return "ID";
+            case 1:
+                return "Nombre";
+            case 2:
+                return "Fecha Nacimiento";
+            case 3:
+                return "Correo";
+            case 4: 
+                return "Cedula";
+            case 5: 
+                return "Telefono";
+            case 6: 
+                return "Etnia";
+            case 7: 
+                return "Titulo de bachiller";
+            case 8: 
+                return "Provincia";
+            default:
+                return null;
+        }
+    }
+
+   
+
+}
