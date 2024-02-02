f16897f37e19a782b2cec76c9ef912ec4242291d
Medina23334444 <139515119+Medina23334444@users.noreply.github.com>
2024-02-02 09:40:08 -0500

    New Changes

--- src/modelLuis/view/Frm_ListaAsistencia.java
+++ src/modelLuis/view/Frm_ListaAsistencia.java
@@ -1,18 +1,22 @@
 package modelLuis.view;
+
 import com.formdev.flatlaf.FlatDarkLaf;
 import java.util.Objects;
 import javax.swing.UIManager;
 import model.Asignatura;
 import model.Cursa;
 import model.Horario;
+import model.Matricula;
 import modelLuis.controller.ControllerAsignatura;
 import modelLuis.controller.ControllerCursa;
 import modelLuis.controller.ControllerHorario;
+import modelLuis.controller.ControllerMatricula;
 import modelLuis.tablas.ModelTableAsistenciaDocente;
+import modelLuis.view.util.Util_VistaLinked1_Asistencia;
 import tda_listas.ListaEnlazada;
 import tda_listas.exceptions.VacioExceptions;
 
-public class Frm_DocenteListaAsistencia extends javax.swing.JFrame {
+public class Frm_ListaAsistencia extends javax.swing.JFrame {
 
     private ListaEnlazada<Horario> horarios = new ListaEnlazada<>();
     private ControllerAsignatura controlerAsignatura = new ControllerAsignatura();
@@ -23,22 +27,30 @@ public class Frm_DocenteListaAsistencia extends javax.swing.JFrame {
     private ModelTableAsistenciaDocente a = new ModelTableAsistenciaDocente();
     private Integer id = 1;
 
-    public Frm_DocenteListaAsistencia() {
+    public Frm_ListaAsistencia() {
         initComponents();
-        cargarTabla();
+
         this.setLocationRelativeTo(null);
         this.setResizable(false);
     }
 
-    public void cargarTabla() {
-        try {
-            getHorario();
-        } catch (Exception e) {
+    private void cargarCombos() throws VacioExceptions {
+        ControllerCursa cc = new ControllerCursa();
+        ControllerAsignatura ca = new ControllerAsignatura();
+        ControllerMatricula cm = new ControllerMatricula();
+        ListaEnlazada<Asignatura> listaAsig = new ListaEnlazada<>();
+        ListaEnlazada<Cursa> listaCursa = new ListaEnlazada<>();
+        ListaEnlazada<Matricula> listMatr = new ListaEnlazada<>();
+        listaCursa = cc.busquedaBinaria(cc.list_All(), id.toString(), "id_docente", "quicksort", 0);
+        for (Cursa cursa : listaCursa) {
+            Matricula matricula = cm.busquedaBinaria2(cm.list_All(), cursa.getIdMatricula().toString(), "id");
+            Asignatura asig = ca.busquedaBinaria2(ca.list_All(), cursa.getIdAsignatura().toString(), "id", 0);
+            listaAsig.add(asig);
+            listMatr.add(matricula);
         }
-        a.setHorarios(horarios);
-        a.setListaAsis(listaAsis);
-        tblDocente.setModel(a);
-        tblDocente.updateUI();
+        Util_VistaLinked1_Asistencia.setListaMatr(listMatr);
+        Util_VistaLinked1_Asistencia.setListaAsig(listaAsig);
+        Util_VistaLinked1_Asistencia.setListaCursa(listaCursa);
     }
 
     public void getCursa() throws VacioExceptions {
@@ -53,40 +65,15 @@ public class Frm_DocenteListaAsistencia extends javax.swing.JFrame {
         }
     }
 
-    public void getHorario() throws VacioExceptions {
-        getCursa();
-        for (Asignatura asig : listaAsis) {
-            String id_asi = asig.getId().toString();
-            ListaEnlazada<Horario> listaHorarios = controlHorario.busquedaBinaria(controlHorario.list_All(), id_asi, "id_asignatura", "quicksort");
-            for (Horario horario : listaHorarios) {
-                if (!isHorarioAgregado(horario.getId())) {
-                    if (Objects.equals(horario.getIdAsignatura(), asig.getId())) {
-                        horarios.add(horario);
-                    }
-                }
-            }
-        }
-        System.out.println(horarios.getSize());
-    }
-
-    private boolean isHorarioAgregado(Integer horarioId) {
-        for (Horario horario : horarios) {
-            if (Objects.equals(horario.getId(), horarioId)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         bg_panel = new javax.swing.JPanel();
         roundPanel1 = new plantilla.swing.RoundPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        tblDocente = new javax.swing.JTable();
-        menu_Docente1 = new plantilla.components.Menu_Docente();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jComboBox2 = new javax.swing.JComboBox<>();
+        jComboBox3 = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -97,23 +84,16 @@ public class Frm_DocenteListaAsistencia extends javax.swing.JFrame {
         roundPanel1.setBackground(new java.awt.Color(51, 51, 51));
         roundPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        tblDocente.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane1.setViewportView(tblDocente);
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        roundPanel1.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 70, 170, -1));
 
-        roundPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 50, -1, -1));
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        roundPanel1.add(jComboBox2, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 70, 180, -1));
+
+        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        roundPanel1.add(jComboBox3, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 70, 160, -1));
 
         bg_panel.add(roundPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 10, 1040, 620));
-        bg_panel.add(menu_Docente1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, -1));
 
         getContentPane().add(bg_panel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1280, 640));
 
@@ -135,16 +115,16 @@ public class Frm_DocenteListaAsistencia extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
-            new Frm_DocenteListaAsistencia().setVisible(true);
+            new Frm_ListaAsistencia().setVisible(true);
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel bg_panel;
-    private javax.swing.JScrollPane jScrollPane1;
-    private plantilla.components.Menu_Docente menu_Docente1;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> jComboBox2;
+    private javax.swing.JComboBox<String> jComboBox3;
     private plantilla.swing.RoundPanel roundPanel1;
-    private javax.swing.JTable tblDocente;
     // End of variables declaration//GEN-END:variables
 
 }
--- src/modelLuis/view/util/Util_VistaLinked1_Asistencia.java
+++ src/modelLuis/view/util/Util_VistaLinked1_Asistencia.java
@@ -13,11 +13,8 @@ import modelLuis.controller.ControllerCursa;
 import modelLuis.controller.ControllerEstudiante;
 import modelLuis.controller.ControllerHorario;
 import modelLuis.controller.ControllerMatricula;
-<<<<<<< HEAD
 import modulo_1.inicio_sesion.controller.DocenteController;
 import modulo_1.periodo_academico.controller.PeriodoAcController;
-=======
->>>>>>> master
 import tda_listas.ListaEnlazada;
 import tda_listas.exceptions.VacioExceptions;
 
@@ -145,7 +142,7 @@ public class Util_VistaLinked1_Asistencia {
     public static void setListaMatr(ListaEnlazada<Matricula> aListaMatr) {
         listaMatr = aListaMatr;
     }
-<<<<<<< HEAD
+
 
     public static void cargaEstudiante(JComboBox cbxmarca) throws VacioExceptions {
         ControllerEstudiante ac = new ControllerEstudiante();
@@ -194,9 +191,5 @@ public class Util_VistaLinked1_Asistencia {
     public static Docente getComboDocente(JComboBox cbx) {
         return (Docente) cbx.getSelectedItem();
     }
-=======
-    
-    
->>>>>>> master
 
 }
